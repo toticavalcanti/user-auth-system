ffeed4fbd83f6d27021cb44f173241004916c5e1
toticavalcanti <toticavalcanti@hotmail.com>
2024-09-23 21:30:23 -0300

    update backend\fiber-project\database\connect.go

--- backend/fiber-project/database/connect.go
+++ backend/fiber-project/database/connect.go
@@ -13,11 +13,13 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	// Obtém a variável de ambiente para a conexão DSN
 	dsn := os.Getenv("DB_DSN")
 	if dsn == "" {
 		log.Fatal("DB_DSN environment variable is not set")
 	}
 
+	// Abre a conexão com o banco de dados
 	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
@@ -25,6 +27,7 @@ func Connect() {
 
 	DB = connection
 
+	// Faz a migração automática dos modelos de User e PasswordReset
 	err = connection.AutoMigrate(&models.User{}, &models.PasswordReset{})
 	if err != nil {
 		log.Fatalf("Failed to auto migrate: %v", err)
