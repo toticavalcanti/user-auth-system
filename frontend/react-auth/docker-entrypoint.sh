b58d152d9622c0870acf706a4c273ad0c21e185f
toticavalcanti <toticavalcanti@hotmail.com>
2024-10-02 20:46:37 -0300

    fix frontend\react-auth\docker-entrypoint.sh

--- frontend/react-auth/docker-entrypoint.sh
+++ frontend/react-auth/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Exibir variáveis de ambiente
+echo "Valor de REACT_APP_API_URL: $REACT_APP_API_URL"
+
+# Verificar o diretório onde o config.js será criado
+ls -ld /usr/share/nginx/html
+
 # Substituir as variáveis de ambiente no arquivo /config.js no momento da inicialização
 echo "Substituindo variáveis de ambiente no /config.js..."
 
@@ -8,6 +14,10 @@ window._env_ = {
   REACT_APP_API_URL: "$REACT_APP_API_URL"
 };
 EOF
+
+# Verificar se o arquivo foi criado
+echo "Verificando se o config.js foi criado..."
+ls /usr/share/nginx/html/config.js
 
 # Iniciar o Nginx
 echo "Iniciando o Nginx..."
