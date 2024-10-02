e051e4992b7104205d168fc25002a3b6449d10b2
toticavalcanti <toticavalcanti@hotmail.com>
2024-10-02 15:12:33 -0300

    update Dockerfile-frontend to fix nginx

--- frontend/react-auth/docker-entrypoint.sh
+++ frontend/react-auth/docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Substituir as variáveis de ambiente no arquivo /config.js no momento da inicialização
-echo "Substituindo variáveis de ambiente no /config.js..."
-
-envsubst < /usr/share/nginx/html/config.js > /usr/share/nginx/html/config.js.temp && mv /usr/share/nginx/html/config.js.temp /usr/share/nginx/html/config.js
+# Substituir variáveis de ambiente no arquivo de configuração do Nginx
+envsubst '\$REACT_APP_API_URL' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 
 # Iniciar o Nginx
-echo "Iniciando o Nginx..."
-exec "$@"
+nginx -g 'daemon off;'
