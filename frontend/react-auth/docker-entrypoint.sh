04ce6fb169dfcfcbb3cf60e55389dad12a88c962
toticavalcanti <toticavalcanti@hotmail.com>
2024-10-03 22:29:50 -0300

    delete custom-entrypoit

--- frontend/react-auth/docker-entrypoint.sh
+++ frontend/react-auth/docker-entrypoint.sh
@@ -1,26 +1,54 @@
 #!/bin/sh
+# vim:sw=4:ts=4:et
+
 set -e
 
-# Debugging echo
-echo "Entrypoint script is running..."
-
-# Verificar se a variável de ambiente foi passada corretamente
-echo "Valor de REACT_APP_API_URL: '${REACT_APP_API_URL}'"
+entrypoint_log() {
+    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
 
 # Criar o arquivo config.js com as variáveis de ambiente
-echo "Criando o arquivo /usr/share/nginx/html/config.js..."
+echo "Substituindo variáveis de ambiente no /usr/share/nginx/html/config.js..."
 cat <<EOL > /usr/share/nginx/html/config.js
 window._env_ = {
   REACT_APP_API_URL: '${REACT_APP_API_URL}'
 };
 EOL
+echo "Arquivo config.js criado com sucesso!"
 
-# Verificar se o arquivo foi criado corretamente
-if [ -f /usr/share/nginx/html/config.js ]; then
-    echo "Arquivo config.js criado com sucesso!"
-else
-    echo "Falha ao criar o arquivo config.js!"
+if [ "$1" = "nginx" ] || [ "$1" = "nginx-debug" ]; then
+    if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+        entrypoint_log "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
+
+        entrypoint_log "$0: Looking for shell scripts in /docker-entrypoint.d/"
+        find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+            case "$f" in
+                *.envsh)
+                    if [ -x "$f" ]; then
+                        entrypoint_log "$0: Sourcing $f";
+                        . "$f"
+                    else
+                        entrypoint_log "$0: Ignoring $f, not executable";
+                    fi
+                    ;;
+                *.sh)
+                    if [ -x "$f" ]; then
+                        entrypoint_log "$0: Launching $f";
+                        "$f"
+                    else
+                        entrypoint_log "$0: Ignoring $f, not executable";
+                    fi
+                    ;;
+                *) entrypoint_log "$0: Ignoring $f";;
+            esac
+        done
+
+        entrypoint_log "$0: Configuration complete; ready for start up"
+    else
+        entrypoint_log "$0: No files found in /docker-entrypoint.d/, skipping configuration"
+    fi
 fi
 
-# Continuar com o processo de inicialização do nginx
 exec "$@"
