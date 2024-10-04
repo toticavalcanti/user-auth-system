d0a31cacc4e4a60e96aa067f8ff941f5f8a7672c
toticavalcanti <toticavalcanti@hotmail.com>
2024-10-04 11:34:20 -0300

    create config-overrides

--- frontend/react-auth/config.ts
+++ frontend/react-auth/config.ts
@@ -1,5 +1,4 @@
 // frontend/react-auth/src/config.ts
-import process from 'process';
 // Define a interface para as variáveis de ambiente
 interface EnvConfig {
     REACT_APP_API_URL?: string;
@@ -22,9 +21,15 @@ interface EnvConfig {
   }
   
   const config: Config = {
-    apiUrl: window._env_?.REACT_APP_API_URL || process.env.REACT_APP_API_URL || 'http://localhost:3000',
-    featureFlagNewUi: window._env_?.REACT_APP_FEATURE_FLAG_NEW_UI === 'true' || process.env.REACT_APP_FEATURE_FLAG_NEW_UI === 'true',
-    logLevel: window._env_?.REACT_APP_LOG_LEVEL || process.env.REACT_APP_LOG_LEVEL || 'error'
+    // Use apenas window._env_ para acessar as variáveis de ambiente definidas
+    apiUrl: window._env_?.REACT_APP_API_URL || 'http://localhost:3000',
+    
+    // Converte a string 'true' para boolean
+    featureFlagNewUi: window._env_?.REACT_APP_FEATURE_FLAG_NEW_UI === 'true',
+  
+    // Define o nível de log
+    logLevel: window._env_?.REACT_APP_LOG_LEVEL || 'error'
   };
   
   export default config;
+  
