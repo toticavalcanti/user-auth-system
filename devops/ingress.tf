894703cf3af4689021e75a2d5e7d0b11da67a534
toticavalcanti <toticavalcanti@hotmail.com>
2025-04-02 18:15:53 -0300

    email sent

--- devops/ingress.tf
+++ devops/ingress.tf
@@ -2,23 +2,38 @@ resource "kubernetes_ingress_v1" "app_ingress" {
   metadata {
     name = "app-ingress"
     annotations = {
-      "kubernetes.io/ingress.class"                    = "nginx"
-      "nginx.ingress.kubernetes.io/ssl-redirect"       = "false"
-      "nginx.ingress.kubernetes.io/use-regex"          = "true"
-      "nginx.ingress.kubernetes.io/rewrite-target"     = "/$1"
+      "kubernetes.io/ingress.class"                 = "nginx"
+      "nginx.ingress.kubernetes.io/ssl-redirect"    = "false"
+      "nginx.ingress.kubernetes.io/proxy-body-size" = "8m"
     }
   }
 
   spec {
+    ingress_class_name = "nginx"
+
     rule {
       http {
+        # Rotas da API
         path {
-          path      = "^/(api|login|register|logout|forgot|reset|user)(/|$)(.*)"
-          path_type = "ImplementationSpecific"
-
+          path      = "/api"
+          path_type = "Prefix"
           backend {
             service {
-              name = "auth-api-service"
+              name = kubernetes_service.auth_api.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+
+        # Rotas de autenticação
+        path {
+          path      = "/forgot"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_api.metadata[0].name
               port {
                 number = 3000
               }
@@ -27,12 +42,91 @@ resource "kubernetes_ingress_v1" "app_ingress" {
         }
 
         path {
-          path      = "/(.*)"
-          path_type = "ImplementationSpecific"
-
+          path      = "/reset"
+          path_type = "Prefix"
           backend {
             service {
-              name = "auth-ui-service"
+              name = kubernetes_service.auth_api.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/register"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_api.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/login"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_api.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/logout"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_api.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/user"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_api.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+
+        # Arquivos estáticos
+        path {
+          path      = "/static"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_ui.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+
+        # Rota padrão
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.auth_ui.metadata[0].name
               port {
                 number = 80
               }
